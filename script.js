b7cfde075d6eaaffaf224a1516bb9bf94518f710
Augustofabg <augustofabg@gmail.com>
2025-01-08 15:02:01 -0300

    Jokenpô

--- script.js
+++ script.js
@@ -1,103 +1,36 @@
 const jogadas = document.querySelectorAll(".tesoura, .papel, .pedra");
 let Ponts1 = document.querySelector(".number1");
 let Ponts2 = document.querySelector(".number2");
+let result = document.querySelector(".resultado");
+let voce = document.querySelector(".voce");
+let oponente = document.querySelector(".oponente");
+let vencedor = document.querySelector(".vencedor");
+
+const resultados = {
+  tesoura: ["✌️", "✊", "✋"],
+  papel: ["✋", "✌️", "✊"],
+  pedra: ["✊", "✋", "✌️"]
+};
+
+const mensagens = ["Empate", "Oponente venceu", "Você venceu"];
+const margens = ["-41px", "-100px", "-70px"];
+const paddings = ["95px", "43px", "65px"];
 
 jogadas.forEach((jogada) => {
   jogada.addEventListener("click", () => {
-    if (jogada.classList.contains("tesoura")) {
-      mathrandom = Math.floor(Math.random() * 3);
-      if (mathrandom == 0) {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 0;
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 0;
-        voce.innerHTML = "✌️";
-        oponente.innerHTML = "✌️";
-        vencedor.innerHTML = "Empate";
-        vencedor.style.marginLeft = "-41px";
-        result.style.paddingRight = "95px";
-        result.style.display = "flex";
-      } else if (mathrandom == 1) {
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 1;
-        voce.innerHTML = "✌️";
-        oponente.innerHTML = "✊";
-        vencedor.innerHTML = "Oponente venceu";
-        vencedor.style.marginLeft = "-100px";
-        result.style.paddingRight = "43px";
-        result.style.display = "flex";
-      } else {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 1;
-        voce.innerHTML = "✌️";
-        oponente.innerHTML = "✋";
-        vencedor.innerHTML = "Você venceu";
-        vencedor.style.marginLeft = "-72px";
-        result.style.paddingRight = "65px";
-        result.style.display = "flex";
-      }
-    }
-
-    if (jogada.classList.contains("papel")) {
-      mathrandom = Math.floor(Math.random() * 3);
-      if (mathrandom == 0) {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 0;
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 0;
-        voce.innerHTML = "✋";
-        oponente.innerHTML = "✋";
-        vencedor.innerHTML = "Empate";
-        vencedor.style.marginLeft = "-41px";
-        result.style.paddingRight = "95px";
-        result.style.display = "flex";
-      } else if (mathrandom == 1) {
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 1;
-        voce.innerHTML = "✋";
-        oponente.innerHTML = "✌️";
-        vencedor.innerHTML = "Oponente venceu";
-        vencedor.style.marginLeft = "-100px";
-        result.style.paddingRight = "43px";
-        result.style.display = "flex";
-      } else {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 1;
-        voce.innerHTML = "✋";
-        oponente.innerHTML = "✊";
-        vencedor.innerHTML = "Você venceu";
-        vencedor.style.marginLeft = "-72px";
-        result.style.paddingRight = "65px";
-        result.style.display = "flex";
-      }
-    }
-
-    if (jogada.classList.contains("pedra")) {
-      mathrandom = Math.floor(Math.random() * 3);
-      if (mathrandom == 0) {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 0;
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 0;
-        voce.innerHTML = "✊";
-        oponente.innerHTML = "✊";
-        vencedor.innerHTML = "Empate";
-        vencedor.style.marginLeft = "-41px";
-        result.style.paddingRight = "95px";
-        result.style.display = "flex";
-        
-      } else if (mathrandom == 1) {
-        Ponts2.innerHTML = parseInt(Ponts2.innerHTML) + 1;
-        voce.innerHTML = "✊";
-        oponente.innerHTML = "✋";
-        vencedor.innerHTML = "Oponente venceu";
-        vencedor.style.marginLeft = "-100px";
-        result.style.paddingRight = "43px";
-        result.style.display = "flex";
-      } else {
-        Ponts1.innerHTML = parseInt(Ponts1.innerHTML) + 1;
-        voce.innerHTML = "✊";
-        oponente.innerHTML = "✌️";
-        vencedor.innerHTML = "Você venceu";
-        vencedor.style.marginLeft = "-72px";
-        result.style.paddingRight = "65px";
-        result.style.display = "flex";
-      }
-    }
+    let mathrandom = Math.floor(Math.random() * 3);
+    let escolha = jogada.classList[0];
+    
+    Ponts1.innerHTML = mathrandom === 2 ? parseInt(Ponts1.innerHTML) + 1 : Ponts1.innerHTML;
+    Ponts2.innerHTML = mathrandom === 1 ? parseInt(Ponts2.innerHTML) + 1 : Ponts2.innerHTML;
+    
+    voce.innerHTML = resultados[escolha][0];
+    oponente.innerHTML = resultados[escolha][mathrandom];
+    
+    vencedor.innerHTML = mensagens[mathrandom];
+    vencedor.style.marginLeft = margens[mathrandom];
+    result.style.paddingRight = paddings[mathrandom];
+    result.style.display = "flex";
   });
 });
 
-let = result = document.querySelector(".resultado");
-let = voce = document.querySelector(".voce");
-let = oponente = document.querySelector(".oponente");
-let = vencedor = document.querySelector(".vencedor");
